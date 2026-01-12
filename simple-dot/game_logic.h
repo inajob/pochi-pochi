aa4677a83b883d2e78ec342c4cd8e8889767c944
inajob <ina_job@hotmail.com>
2026-01-12 10:28:31 +0900

    fix

--- simple-dot/game_logic.h
+++ simple-dot/game_logic.h
@@ -49,7 +49,6 @@ struct GameState {
     int button_down_frames;
     bool was_button_pressed_last_frame;
     bool game_switched_on_long_press;
-    bool brightness_adjusted_on_very_long_press;
     bool ignore_input_until_release;
     int score;
     int frame_count;
